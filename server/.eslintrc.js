683ae73a1500fd89d59422b6e1f42fd549407cb3
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-07-08 02:00:03 +0530

    feat: express setup

---
+++ server/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'linebreak-style': 0,
+    'comma-dangle': [2, 'always-multiline'],
+  },
+};
