14d58f13890230183b82cab90ede91650897cca0
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-07-08 23:44:30 +0530

    feat: db connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const middlewares = require('./middlewares');
 
 const app = express();
 
+// DB Connection
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  .then(() => console.log('🗃 Connected to DB...'))
+  .catch((error) => console.log(error));
+
 // Middlewares
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN,
 }));
 
 app.get('/', (req, res) => {
@@ -28,5 +35,5 @@ app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`🚀 Listening at http://localhost:${port}`);
 });
